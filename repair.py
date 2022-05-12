a7ccd5313edca13190db1ad37c64f022d5cafbe4
PavelBelka <58385396+PavelBelka@users.noreply.github.com>
2022-05-12 20:11:01 +0300

    added new methods recovery

--- repair.py
+++ repair.py
@@ -2,19 +2,32 @@ import queue
 
 class Repair:
     def __init__(self, type_queue):
-        if type_queue == 'FIFO':
+        self.type_queue = type_queue
+        if self.type_queue == 'FIFO':
             self.queue = queue.Queue()
-        elif type_queue == 'LIFO':
+        elif self.type_queue == 'LIFO':
             self.queue = queue.LifoQueue()
         else:
             self.queue = queue.PriorityQueue()
 
     def add_task(self, task):
-        self.queue.put(task)
+        if self.type_queue == "FAST_FIRST":
+            self.queue.put((task[2], task))
+        elif self.type_queue == "LONG_FIRST":
+            self.queue.put((-1 * task[2], task))
+        else:
+            self.queue.put(task)
 
     def get_task(self):
         if not self.queue.empty():
-            return self.queue.get()
+            if self.type_queue == "FAST_FIRST":
+                task = self.queue.get()
+                return task[1]
+            elif self.type_queue == "LONG_FIRST":
+                task = self.queue.get()
+                return task[1]
+            else:
+                return self.queue.get()
 
     def check_empty_queue(self):
         if self.queue.empty():
