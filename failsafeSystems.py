6b1db01c9bce87b3ccd4a8f9b954037159cc0a73
PavelBelka <58385396+PavelBelka@users.noreply.github.com>
2022-05-13 23:49:10 +0300

    fix bugs

--- failsafeSystems.py
+++ failsafeSystems.py
@@ -1,20 +1,27 @@
-import sys, os.path
+import os
+import sys
+
+from PyQt6.QtWidgets import QApplication
 
 from core import Core
-from PyQt6.QtWidgets import QApplication
+from report import Report
 from settings import Settings
+
 
 def main():
     check_file_settings = os.path.exists('settings.yaml')
     settings = Settings()
+    report = Report()
+    app = QApplication(sys.argv)
     if check_file_settings is True:
         settings.read_settings('settings.yaml')
     else:
         settings.create_settings('settings.yaml')
-    app = QApplication(sys.argv)
-    presenter = Core(settings)
-    presenter.window_show()
+    core = Core(settings, report)
+    core.window_show()
+
     app.exec()
+
 
 if __name__ == '__main__':
     main()
--- report.py
+++ report.py
@@ -1,6 +1,4 @@
-import numpy
 import numpy as np
-from numpy import average, max, min
 
 class Report:
     def __init__(self):
@@ -14,31 +12,31 @@ class Report:
         self.num_bars = 100
 
     def calculation_min_time(self, data):
-        self.min_time = min(data)
+        self.min_time = np.min(data)
 
     def calculation_max_time(self, data):
-        self.max_time = max(data)
+        self.max_time = np.max(data)
 
     def calculation_average_time(self, data):
-        self.average_time_failure = average(data)
+        self.average_time_failure = np.average(data)
 
     def calculation_average_time_repair(self, data):
-        self.average_time_repair = average(data)
+        self.average_time_repair = np.average(data)
 
     def calculation_coefficient_ready(self):
         self.coefficient_ready = self.average_time_failure / (self.average_time_failure + self.average_time_repair)
 
     def calculate_histogram(self, data, min_data = None, max_data = None, internal = True, repair = False):
         if internal:
-            minimum = min(data)
-            maximum = max(data)
+            minimum = np.min(data)
+            maximum = np.max(data)
         else:
             minimum = min_data
             maximum = max_data
         if repair:
-            self.histogram_repair = numpy.histogram(data, self.num_bars, (minimum, maximum), density=False)
+            self.histogram_repair = np.histogram(data, self.num_bars, (minimum, maximum), density=False)
         else:
-            self.histogram_failure = numpy.histogram(data, self.num_bars, (minimum, maximum), density=False)
+            self.histogram_failure = np.histogram(data, self.num_bars, (minimum, maximum), density=False)
 
     def get_data_result(self):
         return [self.min_time, self.max_time, self.average_time_failure, self.average_time_repair, self.coefficient_ready]
